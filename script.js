4f1212d5a772ea5692a6edf4983488b357564c7e
Luigi Mendes Queiroz <luigi.jackson@gmail.com>
2021-07-23 03:29:17 -0300

    Feature: Fineshed getTodo async function and content transformed into a new Array

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 async function getTodo(){
+
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await response.json();
+  
+    console.log(data)
+  } catch (err) {
+    console.log(err);
+  }
+
 }
 
 async function printTodo(){
-
+  
 }
+
+getTodo()
