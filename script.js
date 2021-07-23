9ac6bd5b056dc73c749a773fb5ce43ac8516d7dd
Luigi Mendes Queiroz <luigi.jackson@gmail.com>
2021-07-23 15:03:42 -0300

    Feature: Finished printUsers async function to fill the table with the content of the fetch requisition and added the event on the button to fill the table once clicked.

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-async function getTodo(){
+const printButton = document.querySelector(".button");
+const tBody = document.querySelector('tbody');
+
+async function getUsers(){
 
   try {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
     const data = await response.json();
-  
-    console.log(data)
+    
+    return data
   } catch (err) {
-    console.log(err);
+    console.log(err.message);
+  }
+}
+
+async function printUsers(){
+  const data = await getUsers();
+
+  if (!data) return;
+
+  data.forEach(data =>{
+    const row = document.createElement('tr');
+    const fields = ['email', 'id', 'name', 'phone', 'username', 'website'];
+
+    fields.forEach(field => {
+      const content = document.createElement('td');
+
+      content.textContent = data[field];
+
+      row.appendChild(content);
+    })
+    tBody.appendChild(row);
+  })
+
+
   }
 
-}
+printButton.addEventListener("click", printUsers);
 
-async function printTodo(){
-  
-}
-
-getTodo()
